cd50fbf2b560e8ede100ea9453e586597108e81f
snoopingh <snoopingh@naver.com>
2022-12-14 15:44:06 +0900

    xunit 따라하기

---
+++ src/xunit/TestCaseTest.java
@@ -0,0 +1,59 @@
+package xunit;
+
+import xunit.annotation.Test;
+
+public class TestCaseTest extends TestCase {
+    public TestCaseTest(String name) {
+        super(name);
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestCaseTest.class);
+    }
+
+    @Override
+    public void setUp() {
+    }
+
+    @Test
+    public void templateMethod() {
+        WasRun wasRun = new WasRun("testMethod");
+        TestResult result = new TestResult();
+        wasRun.run(result);
+        Assert.assertEquals("setUp testMethod tearDown", wasRun.log);
+    }
+
+    @Test
+    public void result() {
+        WasRun wasRun = new WasRun("testMethod");
+        TestResult result = new TestResult();
+        wasRun.run(result);
+        Assert.assertEquals("1 run, 0 failed", result.getSummary());
+    }
+
+    @Test
+    public void testFailedResultFormatting() {
+        TestResult result = new TestResult();
+        result.testStarted();
+        result.testFailed();
+        Assert.assertEquals("1 run, 1 failed", result.getSummary());
+    }
+
+    @Test
+    public void failedResult() {
+        WasRun wasRun = new WasRun("testBrokenMethod");
+        TestResult result = new TestResult();
+        wasRun.run(result);
+        Assert.assertEquals("1 run, 1 failed", result.getSummary());
+    }
+
+    @Test
+    public void testSuite() {
+        TestSuite suite = new TestSuite();
+        suite.add(new WasRun("testMethod"));
+        suite.add(new WasRun("testBrokenMethod"));
+        TestResult result = new TestResult();
+        suite.run(result);
+        Assert.assertEquals("2 run, 1 failed", result.getSummary());
+    }
+}
---
+++ src/xunit/TestSuite.java
@@ -0,0 +1,39 @@
+package xunit;
+
+import org.springframework.core.annotation.AnnotationUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestSuite implements Test {
+    List<Test> tests = new ArrayList<>();
+
+    public TestSuite(Class<? extends TestCase> testClass) {
+        Arrays.stream(testClass.getMethods())
+                .filter(m -> AnnotationUtils.findAnnotation(m, xunit.annotation.Test.class) != null)
+                .forEach(m ->
+                        {
+                            try {
+                                add(testClass.getConstructor(String.class).newInstance(m.getName()));
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                );
+    }
+
+    public TestSuite() {
+
+    }
+
+    public void add(Test test) {
+        tests.add(test);
+    }
+
+    public void run(TestResult result) {
+        tests.forEach(t -> {
+            t.run(result);
+        });
+    }
+}
